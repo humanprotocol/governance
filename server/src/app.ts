4046624f4c024bdc4a062c46461f06eba48e4a94
portuu3 <adrian.portugues.mas@gmail.com>
2025-02-21 13:04:49 +0100

    add cors support

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import proposalRoutes from './routes/proposalRoutes';
 
 const app = express();
 const port = process.env.NODE_PORT || 8080;
+const corsOptions = {
+    origin: process.env.ALLOWED_CORS_ORIGIN || '*',
+};
 
+app.use(cors(corsOptions));
 app.use(proposalRoutes);
 
 app.listen(port, () => {
